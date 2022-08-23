d094681172057a60d93ce398b5eba8388c74251d
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-08-23 07:50:38 +0100

    Completed the item history page
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/itemhistory.blade.php
+++ resources/views/itemhistory.blade.php
@@ -81,21 +81,17 @@
                                                                             <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                                                                             <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                                                                         </svg>
-                                                                        <span class="truncate">ricardo.cooper@example.com</span>
+                                                                        <span class="truncate">{{$d->User->email}}</span>
                                                                     </p>
                                                                 </div>
                                                                 <div class="hidden md:block">
                                                                     <div>
-                                                                        <p class="text-sm text-gray-900">
-                                                                            Applied on
-                                                                            <time datetime="2020-01-07">January 7, 2020</time>
-                                                                        </p>
                                                                         <p class="mt-2 flex items-center text-sm text-gray-500">
                                                                             <!-- Heroicon name: solid/check-circle -->
                                                                             <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                                                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                                                                             </svg>
-                                                                            Completed phone screening
+                                                                            Returned on {{$d->updated_at->format('d/m/y')}}
                                                                         </p>
                                                                     </div>
                                                                 </div>
