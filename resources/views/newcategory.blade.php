948214ae9514973a1c0eb4742a43ee26510f8e91
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-08-28 11:12:47 +0100

    Created the form for creating a new category
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/newcategory.blade.php
+++ resources/views/newcategory.blade.php
@@ -8,6 +8,50 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
+            <!--
+  This example requires Tailwind CSS v2.0+
+
+  This example requires some changes to your config:
+
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+            <form class="space-y-6" action="{{route('category.create')}}" method="post">
+                <div class="bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6">
+                    <div class="md:grid md:grid-cols-3 md:gap-6">
+                        <div class="md:col-span-1">
+                            <h3 class="text-lg font-medium leading-6 text-gray-900">Category</h3>
+                            <p class="mt-1 text-sm text-gray-500">Insert information about the new category</p>
+                        </div>
+                        <div class="mt-5 space-y-6 md:mt-0 md:col-span-2">
+                            <div class="grid grid-cols-3 gap-6">
+                                <div class="col-span-3 sm:col-span-2">
+                                    <label for="company-website" class="block text-sm font-medium text-gray-700"> Name of category </label>
+                                    <div class="mt-1 flex rounded-md shadow-sm">
+{{--                                        <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"> http:// </span>--}}
+                                        <input type="text" name="category" id="category" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300">
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex justify-end">
+                    <button type="button" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Cancel</button>
+                    <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+                </div>
+            </form>
+
 
         </div>
     </div>
