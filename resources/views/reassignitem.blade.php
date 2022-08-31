a04f4fa4235b49d8bf104c8ad10d9f3003122885
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-08-31 09:09:26 +0100

    Added validation check and modal to reassign item message
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

--- resources/views/reassignitem.blade.php
+++ resources/views/reassignitem.blade.php
@@ -51,17 +51,39 @@
                                     </div>
 
                                 </div>
-                                <div>
-                                    <label for="category"></label><select id="category" name="category" class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                                        @foreach($categories as $c)
-                                            <option value="{{$c->id}}">{{$c->category}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                                    <button type="submit" class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Reassign</button>
-                                    <a href="{{route('category.view', $current->id)}}" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm">Cancel</a>
-                                </div>
+                                @if($categories->count() > 0)
+                                    <div>
+                                        <label for="category"></label><select id="category" name="category" class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+                                            @foreach($categories as $c)
+                                                <option value="{{$c->id}}">{{$c->category}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                                        <button type="submit" class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Reassign</button>
+                                        <a href="{{route('category.view', $current->id)}}" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm">Cancel</a>
+                                    </div>
+
+                                @else
+                                    <div class="pt-5">
+                                        <div class="rounded-md bg-yellow-50 p-4">
+                                            <div class="flex">
+                                                <div class="flex-shrink-0">
+                                                    <!-- Heroicon name: mini/exclamation-triangle -->
+                                                    <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                        <path fill-rule="evenodd" d="M8.485 3.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 3.495zM10 6a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 6zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </div>
+                                                <div class="ml-3">
+                                                    <h3 class="text-sm font-medium text-yellow-800">Action required</h3>
+                                                    <div class="mt-2 text-sm text-yellow-700">
+                                                        <p>In order to reassign a category, another category needs be created for this one to be reassigned.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
                             </div>
                         </form>
                     </div>
