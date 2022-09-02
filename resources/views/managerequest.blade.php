db31b190963eea74121f632479a7ada41c25afcb
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-02 18:48:44 +0100

    Created view to manage requests
    Signed-off-by: WillTheDeveloper <willthedeveloper13@gmail.com>

---
+++ resources/views/managerequest.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Manage request') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <!-- This example requires Tailwind CSS v2.0+ -->
+            <div>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
